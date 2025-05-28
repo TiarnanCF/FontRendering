7863ce6bbdbd031443225efbac03b52280cd38bb
Tiarnán Curran-Feeney <tiarnan.curran-feeney@avamae.co.uk>
2025-05-29 11:48:41 +1200

    Set up initial test to check beziers were built appropriately

--- Tests/Test_BezierBuilder.py
+++ Tests/Test_BezierBuilder.py
@@ -1,5 +1,6 @@
 import unittest
 from FontRender.CorePackages.HelperPackages.BezierHelper.BezierBuilder import BezierBuilder
+from FontRender.CorePackages.HelperPackages.BezierHelper.QuadraticBezier import QuadraticBezier
 import numpy as np
 
 class TestBezierBuilder(unittest.TestCase):
@@ -12,8 +13,22 @@ class TestBezierBuilder(unittest.TestCase):
     BezierBuilder.insert_quadratic_bezier(self.beziers,1,3,-2)
     BezierBuilder.insert_linear_bezier(self.beziers,2,3,1)
 
+    self.validate_beziers()
+
   def test_validate_initial_beziers(self):
     pass
+
+  def validate_beziers(self):
+    next_point = self.beziers[0].x0
+    for bezier in self.beziers:
+      self.assertTrue(bezier.x0 is next_point)
+      next_point = next_point.get_next()
+      if isinstance(bezier, QuadraticBezier):
+        self.assertTrue(bezier.x1 is next_point)
+        next_point = next_point.get_next()
+
+      self.assertTrue(bezier.x2 is next_point)
+
     
 
 if __name__ == '__main__':
