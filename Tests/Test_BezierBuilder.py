c4ae1520ad19a93f20f71c895c12001cdc56dc93
Tiarnán Curran-Feeney <tiarnan.curran-feeney@avamae.co.uk>
2025-05-29 18:21:24 +1200

    Set up some unit tests for BezierBuilder

--- Tests/Test_BezierBuilder.py
+++ Tests/Test_BezierBuilder.py
@@ -1,6 +1,8 @@
 import unittest
 from FontRender.CorePackages.HelperPackages.BezierHelper.BezierBuilder import BezierBuilder
 from FontRender.CorePackages.HelperPackages.BezierHelper.QuadraticBezier import QuadraticBezier
+from FontRender.CorePackages.HelperPackages.BezierHelper.LinearBezier import LinearBezier
+from FontRender.CorePackages.HelperPackages.BezierHelper.Point import Point
 import numpy as np
 
 class TestBezierBuilder(unittest.TestCase):
@@ -15,8 +17,27 @@ class TestBezierBuilder(unittest.TestCase):
 
     self.validate_beziers()
 
-  def test_validate_initial_beziers(self):
-    pass
+  def test_reduce_to_linear_bezier(self):
+    quadratic_bezier = self.beziers[1]
+    linear_bezier = BezierBuilder.reduce_to_linear_bezier(quadratic_bezier)
+    self.assertTrue(linear_bezier.get_x0() is quadratic_bezier.get_x0())
+    self.assertTrue(linear_bezier.get_x2() is quadratic_bezier.get_x2())
+    self.assertTrue(isinstance(linear_bezier, LinearBezier))
+
+    self.beziers[1] = linear_bezier
+    self.validate_beziers()
+
+  def test_enhance_to_quadratic_bezier(self):
+    linear_bezier = self.beziers[0]
+    new_point_x1 = Point(1.3,2,False)
+    quadratic_bezier = BezierBuilder.enhance_to_quadratic_bezier(linear_bezier, new_point_x1)
+    self.assertTrue(linear_bezier.get_x0() is quadratic_bezier.get_x0())
+    self.assertTrue(linear_bezier.get_x2() is quadratic_bezier.get_x2())
+    self.assertTrue(isinstance(quadratic_bezier, QuadraticBezier))
+    self.assertEqual(new_point_x1, quadratic_bezier.get_x1())
+
+    self.beziers[0] = quadratic_bezier
+    self.validate_beziers()
 
   def validate_beziers(self):
     next_point = self.beziers[0].get_x0()
